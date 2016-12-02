2cbbf0fc052774138e9caa52199f3af75aa0920f
Shantanu Singhal <shantanusinghal2709@gmail.com>
2016-12-01 23:54:03 -0600

    added stage 4 report and updated index.html

--- Features.py
+++ Features.py
@@ -1,239 +1,228 @@
 import py_stringmatching as ps
 import csv
 from datetime import datetime, date
-from sklearn import tree, ensemble, svm, naive_bayes, linear_model #.RandomForestClassifier
+from sklearn import tree, ensemble, svm, naive_bayes, linear_model
 from sklearn.metrics import precision_recall_fscore_support
 
-
-timeObj1=datetime.strptime('03:55', '%M:%S').time()
-timeObj2 = datetime.strptime('04:55', '%M:%S').time()
-print max(timeObj1,timeObj2), datetime.combine(date.today(), max(timeObj1,timeObj2)) - datetime.combine(date.today(), min(timeObj1,timeObj2))
-
-#Reading every row of csv into a list
-f = open('SampledData.csv','rb')
-csvRead = csv.reader(f,delimiter=',')
+# Reading every row of csv into a list
+f = open('SampledData.csv', 'rb')
+csvRead = csv.reader(f, delimiter=',')
 sampledList = []
 for row in csvRead:
-	row[0] = int(row[0])
-	row[-1] = int(row[-1])
-	timeObj1=datetime.strptime(row[6].strip(), '%M:%S').time()
-	row[6] = timeObj1
-	timeObj1=datetime.strptime(row[10].strip(), '%M:%S').time()
-	row[10] = timeObj1
-	
-	#l.append(line[:-1])
-	sampledList.append(row)
-	#print row
+    row[0] = int(row[0])
+    row[-1] = int(row[-1])
+    timeObj1=datetime.strptime(row[6].strip(), '%M:%S').time()
+    row[6] = timeObj1
+    timeObj1=datetime.strptime(row[10].strip(), '%M:%S').time()
+    row[10] = timeObj1
+    # iteration #2: trim whitespaces from artist and track labels
+    row[3] = row[3].strip()
+    row[4] = row[4].strip()
+    row[7] = row[7].strip()
+    row[8] = row[8].strip()
+    sampledList.append(row)
 f.close()
 
-#print row[6]
-#print max(row[6],row[10])
-
-#print sampledList
-
-#Converting every row to feature vector
+# Converting every row in to a feature vector
 featList = []
 label = []
+ws = ps.WhitespaceTokenizer()
 for item in sampledList:
-	#print item
-	fi = []
-	
-	jaro1 = ps.Jaro()
-	f1 = jaro1.get_raw_score(item[3],item[7])#Artist- 3,7 - Jaro
-	
-	jaro2 = ps.Jaro()
-	f2 = jaro1.get_raw_score(item[4],item[8])#Trackname -4,8 - Jaro
-	
-	jaro3 = ps.Jaro()
-	f3 = jaro1.get_raw_score(item[5],item[9])#Released Date - 5,9 - Jaro
-	
-	timeObj3 = datetime.strptime('00:00', '%M:%S').time()
-	#print timeObj3 #, item 
-	f4 = (datetime.combine(date.today(), max(item[6],item[10])) - datetime.combine(date.today(), min(item[6],item[10]))).total_seconds()#Time -6,10 - diff/max	
-	f4de =(datetime.combine(date.today(), max(item[6],item[10])) - datetime.combine(date.today(), timeObj3)).total_seconds()
-	f4/= f4de
-	#print f4 
-	#sampledList[item]
-	
-	fi.append(f1)
-	fi.append(f2)
-	fi.append(f3)
-	fi.append(f4)
-	label.append(item[-1])
-	featList.append(fi)
-	
-	
-#print featList
-#print label	
+    fi = []
 
-#H = featList
+    jaro1 = ps.Jaro()
+
+    # iteration #3:
+    # pull the feature value to zero if none of the token pairs from either artist strings have a high
+    # enough similarity score
+    f1 = 0
+    for t1 in ws.tokenize(item[3]):
+        if max([jaro1.get_raw_score(t1, t2) for t2 in ws.tokenize(item[7])]) > .75:
+            f1 = jaro1.get_raw_score(item[3], item[7])
+            break
+
+    # iteration #3:
+    # if the artist doesn't match scale down the track similarity by a factor of 3
+    # and if the track score isn't high enough pull it down to 0
+    jaro2 = ps.Jaro()
+    f2 = jaro1.get_raw_score(item[4], item[8])
+    if f1 == 0:
+        f2 /= 3
+    elif f2 < 0.6:
+        f2 = 0
+
+    # convert to datetime object
+    date1 = datetime.strptime(item[5],'%d-%b-%y')
+    date2 = datetime.strptime(item[9],'%d-%b-%y')
+    timeObj4 = datetime.strptime('00:00:00', '%H:%M:%S').time()
+    # take the difference of the weights
+    dif = datetime.combine(max(date1, date2), timeObj4) - datetime.combine( min(date1, date2), timeObj4)
+    norm = datetime.combine(date.today(), timeObj4)- datetime.combine( min(date1, date2), timeObj4)
+    # normalize the feature value
+    f3 = 1.0*(norm.days-dif.days)/norm.days
+
+    # convert to datetime object
+    timeObj3 = datetime.strptime('00:00', '%M:%S').time()
+    # take the difference of the track time lengths
+    f4 = (datetime.combine(date.today(), max(item[6],item[10])) - datetime.combine(date.today(), min(item[6],item[10]))).total_seconds()#Time -6,10 - diff/max
+    f4de =(datetime.combine(date.today(), max(item[6],item[10])) - datetime.combine(date.today(), timeObj3)).total_seconds()
+    # normalize the feature value
+    f4 /= f4de
+    f4 = 1.0 - f4
+
+    # add the feature values to the feature vector
+    fi.append(f1)
+    fi.append(f2)
+    fi.append(f3)
+    fi.append(f4)
+    label.append(item[-1])
+    featList.append(fi)
+
+
+# split the data-set in to development set (I) and evaluation sets (J)
 Ifeat = featList[0:300]
-#U = Ifeat[0:150]
-#V = Ifeat[150:-1]
 Jfeat = featList[300:-1]
 Ilabel = label[0:300]
-#Ulabel = Ilabel[0:150]
-#Vlabel = Ilabel[150:-1]
 Jlabel = label[300:-1]
 
-#print Ifeat
-#print len(Ilabel)
-# dtErr = 0
-# rfErr = 0
-# svmErr = 0
-# gnbErr =0
-# lrErr = 0
-
-dtTp = 0.0
-dtFp = 0.0
-dtFn = 0.0
-rfTp = 0.0
-rfFp = 0.0
-rfFn = 0.0
-svmTp = 0.0
-svmFp = 0.0
-svmFn = 0.0
-gnbTp = 0.0
-gnbFp = 0.0
-gnbFn = 0.0
-lrTp = 0.0
-lrFp = 0.0
-lrFn = 0.0
-
+# initialize data structures for cross validation
 dtTrue = []
 rfTrue = []
 svmTrue = []
 gnbTrue = []
 lrTrue = []
 
-
-dtTP = 0
-dtFP = 0
-dtFN = 0
-
-rfTP = 0
-rfFP = 0
-rfFN = 0
-
-svmTP = 0
-svmFP = 0
-svmFN = 0
-
-gnbTP = 0
-gnbFP = 0
-gnbFN = 0
-
-lrTP = 0
-lrFP = 0
-lrFN = 0
-
-
+# STEP 2: perform leave-one-out-cross-validation
 for i in range(len(Ifeat)):
-	X = Ifeat[0:i] + Ifeat[i+1:]
-	Y = Ilabel[0:i] + Ilabel[i+1:]
-	#DT
-	dt = tree.DecisionTreeClassifier()
-	dt = dt.fit(X, Y)
-	dtPred = dt.predict([Ifeat[i]])
-	dtTrue.append(dtPred)
-	if dtPred == Ilabel[i]:
-		dtTp += 1
-	elif dtPred == 1:
-		dtFp += 1
-	else:
-		dtFn += 1
-	#print("Number of mislabeled points out of a total %d points : %d" % (len(V),(Vlabel != dtPred).sum()))
+    X = Ifeat[0:i] + Ifeat[i+1:]
+    Y = Ilabel[0:i] + Ilabel[i+1:]
 
+    # decision trees
+    dt = tree.DecisionTreeClassifier()
+    dt = dt.fit(X, Y)
+    dtPred = dt.predict([Ifeat[i]])
+    dtTrue.append(dtPred)
 
-	#RF	
-	rf = ensemble.RandomForestClassifier()
-	rf = rf.fit(X, Y)
-	rfPred = rf.predict([Ifeat[i]])
-	rfTrue.append(rfPred)
-	if rfPred == Ilabel[i]:
-		rfTp += 1
-	elif rfPred == 1:
-		rfFp += 1
-	else:
-		rfFn += 1
-	#print("Number of mislabeled points out of a total %d points : %d" % (len(V),(Vlabel != rfPred).sum()))
+    # random forest
+    rf = ensemble.RandomForestClassifier()
+    rf = rf.fit(X, Y)
+    rfPred = rf.predict([Ifeat[i]])
+    rfTrue.append(rfPred)
 
-	#SVM
-	s1 = svm.SVC()
-	svmFit = s1.fit(X,Y)
-	svmPred = s1.predict([Ifeat[i]])
-	svmTrue.append(svmPred)
-	if svmPred == Ilabel[i]:
-		svmTp += 1
-	elif svmPred == 1:
-		svmFp += 1
-	else:
-		svmFn += 1
-	#print svmPred
-	#print("Number of mislabeled points out of a total %d points : %d" % (len(V),(Vlabel != svmPred).sum()))
+    # support vector machine
+    s1 = svm.SVC()
+    svmFit = s1.fit(X,Y)
+    svmPred = s1.predict([Ifeat[i]])
+    svmTrue.append(svmPred)
 
-	#NB
-	gnb = naive_bayes.GaussianNB()
-	gnb = gnb.fit(X,Y)
-	gnbPred = gnb.predict([Ifeat[i]])
-	gnbTrue.append(gnbPred)
-	if gnbPred == Ilabel[i]:
-		gnbTp += 1
-	elif gnbPred == 1:
-		gnbFp += 1
-	else:
-		gnbFn += 1
-	#print gnbPred
-	#print("Number of mislabeled points out of a total %d points : %d" % (len(V),(Vlabel != gnbPred).sum()))
+    # gaussian naive bayes
+    gnb = naive_bayes.GaussianNB()
+    gnb = gnb.fit(X,Y)
+    gnbPred = gnb.predict([Ifeat[i]])
+    gnbTrue.append(gnbPred)
 
-	#LR
-	lr = linear_model.LogisticRegression()
-	lr = lr.fit(X,Y)
-	lrPred = gnb.predict([Ifeat[i]])
-	lrTrue.append(lrPred)
-	if lrPred == Ilabel[i]:
-		lrTp += 1
-	elif lrPred == 1:
-		lrFp += 1
-	else:
-		lrFn += 1
-	#print lrPred
+    # logistic regression
+    lr = linear_model.LogisticRegression()
+    lr = lr.fit(X,Y)
+    lrPred = gnb.predict([Ifeat[i]])
+    lrTrue.append(lrPred)
 
-#print dtErr, type(dtErr)
-# dtRate = float(dtErr)/len(Ifeat)
-# rfRate = float(rfErr)/len(Ifeat)
-# svmRate = float(svmErr)/len(Ifeat)
-# gnbRate = float(gnbErr)/len(Ifeat)
-# lrRate = float(lrErr)/len(Ifeat)
+# report the scores for all the classifiers
+print "decision tree"
+print precision_recall_fscore_support(Ilabel, dtTrue)
+print "random forest"
+print precision_recall_fscore_support(Ilabel, rfTrue)
+print "svm"
+print precision_recall_fscore_support(Ilabel, svmTrue)
+print "gnb"
+print precision_recall_fscore_support(Ilabel, gnbTrue)
+print "lr"
+print precision_recall_fscore_support(Ilabel, lrTrue)
 
-dtPrecision = dtTp / (dtTp + dtFp)
-dtRecall = dtTp / (dtTp + dtFn)
-dtF1Score = 2 * (dtPrecision * dtRecall) / (dtPrecision + dtRecall)
+# STEP 3: debug random forest classifier
 
-rfPrecision = rfTp / (rfTp + rfFp)
-rfRecall = rfTp / (rfTp + rfFn)
-rfF1Score = 2 * (rfPrecision * rfRecall) / (rfPrecision + rfRecall)
+# split the development set into two halves U and V
+Ufeat = Ifeat[0:len(Ifeat)/2]
+ULabel = Ilabel[0:len(Ifeat)/2]
+Vfeat = Ifeat[len(Ifeat)/2:]
+Vlabel = Ilabel[len(Ifeat)/2:]
+vFeatTrue = []
 
-svmPrecision = svmTp / (svmTp + svmFp)
-svmRecall = svmTp / (svmTp + svmFn)
-svmF1Score = 2 * (svmPrecision * svmRecall) / (svmPrecision + svmRecall)
+# train the classifier on U and evaluate on V
+rfDebug = ensemble.RandomForestClassifier()
+rfDebug = rfDebug.fit(Ufeat, ULabel)
+rfPredDebug = rfDebug.predict(Vfeat)
 
-gnbPrecision = gnbTp / (gnbTp + gnbFp)
-gnbRecall = gnbTp / (gnbTp + gnbFn)
-gnbF1Score = 2 * (gnbPrecision * gnbRecall) / (gnbPrecision + gnbRecall)
+# output the false positives and false negatives in the predictions made by the classifier on V
+print 'DEBUGGING THE NEW MODEL'
+for i in range(len(Vlabel)):
 
-lrPrecision = lrTp / (lrTp + lrFp)
-lrRecall = lrTp / (lrTp + lrFn)
-lrF1Score = 2 * (lrPrecision * lrRecall) / (lrPrecision + lrRecall)
+    vFeatTrue.append(rfPredDebug[i])
 
-# print "dtPrecision: " + str(dtPrecision) + "dtRecall: " + str(dtRecall) + "dtF1: " + str(dtF1Score)
-# print "rfPrecision: " + str(rfPrecision) + "rfRecall: " + str(rfRecall) + " rfF1: " + str(rfF1Score) #+ "sklRfF1: " + precision_recall_fscore_support(rfTrue, Ilabel)
-# print "svmPrecision: " + str(svmPrecision) + "svmRecall: " + str(svmRecall) + " svmF1: " + str(svmF1Score) #+ "sklSvmF1: " + precision_recall_fscore_support(svmTrue, Ilabel)
-# print "gnbPrecision: " + str(gnbPrecision) + "gnbRecall: " + str(gnbRecall) + " gnbF1: " + str(gnbF1Score) #+ "sklGnbF1: " + precision_recall_fscore_support(gnbTrue, Ilabel)
-# print "lrPrecision: " + str(lrPrecision) + "lrRecall: " + str(lrRecall) + " lrF1: " + str(lrF1Score) #+ "sklLrF1: " + precision_recall_fscore_support(lrTrue, Ilabel)
-print precision_recall_fscore_support(Ilabel, dtTrue, average='binary')
-print precision_recall_fscore_support(Ilabel, rfTrue, average='binary')
-print precision_recall_fscore_support(Ilabel, svmTrue, average='binary')
-print precision_recall_fscore_support(Ilabel, gnbTrue, average='binary')
-print precision_recall_fscore_support(Ilabel, lrTrue, average='binary')
+    if Vlabel[i] == 0 and rfPredDebug[i] == 1:
+        print 'Debugging RF -> False Positive: ', sampledList[len(Ufeat) + i],  rfPredDebug[i]
+        print Vfeat[i]
+        print rfDebug.decision_path([Vfeat[i]])
 
+    if Vlabel[i] == 1 and rfPredDebug[i] == 0:
+        print 'Debugging RF -> False Negative: ', sampledList[len(Ufeat) + i], rfPredDebug[i]
+        print Vfeat[i]
+        print rfDebug.decision_path([Vfeat[i]])
+
+# output the precision, recall and F1 score of the classifier during debugging
+print 'predictions on V-features'
+print vFeatTrue
+print 'precision, recall and f1 on V-features'
+print precision_recall_fscore_support(Vlabel, vFeatTrue)
+
+# STEP 4: report the precision, recall and F1 score for all the classifiers on J
+
+# decision tree trained on I and evaluated on J
+dtFinal = tree.DecisionTreeClassifier()
+dtFinal = dtFinal.fit(Ifeat, Ilabel)
+dtFinalTrue = []
+for i in range(len(Jfeat)):
+    dtPredFinal = dtFinal.predict([Jfeat[i]])
+    dtFinalTrue.append(dtPredFinal)
+print "Decision Tree evaluated on J:"
+print precision_recall_fscore_support(Jlabel, dtFinalTrue)
+
+# random forest (best classifier) trained on I and evaluated on J
+rfFinal = ensemble.RandomForestClassifier()
+rfFinal = rfFinal.fit(Ifeat, Ilabel)
+rfFinalTrue = []
+for i in range(len(Jfeat)):
+    rfPredFinal = rf.predict([Jfeat[i]])
+    rfFinalTrue.append(rfPredFinal)
+print "Random Forest evaluated on J:"
+print precision_recall_fscore_support(Jlabel, rfFinalTrue)
+
+# support vector machine trained on I and evaluated on J
+svmFinal = svm.SVC()
+svmFinal = svmFinal.fit(Ifeat, Ilabel)
+svmFinalTrue = []
+for i in range(len(Jfeat)):
+    svmPredFinal = svmFinal.predict([Jfeat[i]])
+    svmFinalTrue.append(svmPredFinal)
+print "Support Vector Machine evaluated on J:"
+print precision_recall_fscore_support(Jlabel, svmFinalTrue)
+
+# gaussian naive bayes trained on I and evaluated on J
+gnbFinal = naive_bayes.GaussianNB()
+gnbFinal = gnbFinal.fit(Ifeat, Ilabel)
+gnbFinalTrue = []
+for i in range(len(Jfeat)):
+    gnbPredFinal = gnbFinal.predict([Jfeat[i]])
+    gnbFinalTrue.append(gnbPredFinal)
+print "Gaussian Naive Bayes evaluated on J:"
+print precision_recall_fscore_support(Jlabel, gnbFinalTrue)
+
+# logistic regression trained on I and evaluated on J
+lrFinal = linear_model.LogisticRegression()
+lrFinal = lrFinal.fit(Ifeat, Ilabel)
+lrFinalTrue = []
+for i in range(len(Jfeat)):
+    lrPredFinal = lrFinal.predict([Jfeat[i]])
+    lrFinalTrue.append(lrPredFinal)
+print "Logistic Regression evaluated on J:"
+print precision_recall_fscore_support(Jlabel, lrFinalTrue)
